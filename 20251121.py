71b93f1bbd9a75bacd4751c442e7bec6b197f0b9
ycchanbi <moon.chegg246@gmail.com>
2025-11-21 09:38:17 +0800

    Update 20251121.py

--- 20251121.py
+++ 20251121.py
@@ -39,8 +39,9 @@ with tab2:
         "Very satisfied with the product quality.",
         "Quick delivery and excellent support."
     ]
+    idx = 1
     for feedback in customer_feedback:
-        st.write(f"1. {feedback}")
+        st.write(f"{idx}, {feedback}")
 with tab3:
     st.write("Content for Market Trends")
     market_trends = {
