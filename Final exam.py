b26314be05fec0a9d4d5569c4144da613aeb45cd
ycchanbi <moon.chegg246@gmail.com>
2025-11-30 15:19:16 +0800

    Update Final exam.py

--- Final exam.py
+++ Final exam.py
@@ -1,20 +1,25 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+from streamlit_option_menu import option_menu
 
+st.title('Hello, Students!')
+st.write('This is your Python Programming course.')
 
-st.title("Streamlit Demo App")
-st.header("User Input Section")
+with st.sidebar:
+    selected=option_menu(
+        menu_title = "Menu",
+        options = ["Home", "About", "Contact"],
+        icons = ["1-circle-fill",
+                 "2-circle-fill",
+                 "3-circle-fill"],
+        menu_icon= "emoji-smile-fill",
+        default_index=0,
+    )
 
-st.write("Please provide your details below:")
+if selected == "Home":
+    st.title(f"Welcome to the {selected} page.")
 
-age = st.number_input("Enter your age:",
-                      min_value=0,
-                      max_value=120,
-                      value=25)
-color = st.selectbox("Choose your favorite color:",
-                     ["Red", "Blue", "Green"])
+if selected == "About":
+    st.title(f"Welcome to the {selected} page.")
 
-if st.button("Submit"):
-    st.success(f"Thank you! Age: {age}, Favorite Color: {color}")
+if selected == "Contact":
+    st.title(f"Welcome to the {selected} page.")
