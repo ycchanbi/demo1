fbfe56dc52c0a57bc6ca95e3854d2ca84237cf4f
ycchanbi <moon.chegg246@gmail.com>
2025-11-30 13:48:56 +0800

    Update Final exam.py

--- Final exam.py
+++ Final exam.py
@@ -12,6 +12,11 @@ st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
 st.write("**Bold Text** and *Italic Text*")
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
+st.write(f"Your age is {age}")
 
 # -------------------------------
 # Sample Data
